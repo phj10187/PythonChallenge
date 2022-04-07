eebac6d9e8d26a370bbba38bf038e5c5693e5a79
hyunji <92680176+phj10187@users.noreply.github.com>
2022-04-07 13:38:48 +0900

    Update 220327.py

--- 220327.py
+++ 220327.py
@@ -1,1 +1,10 @@
-print ("This is a massage") 
+#026
+word = input("Please enter a word: ")
+first = word[0].lower() #소문자 첫글자
+length = len(word) 
+rest = word[1:length]
+if first != "a" and first != "e" and first != "i" and first != "o" and first != "u":
+  newword = rest + first + "ay"
+else:
+  newword = word + "way"
+print(newword.lower())
